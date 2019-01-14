fc004b7b144c386ca7e567e69e349a3a834115d0
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-14 18:40:41 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/ConvertingFromRegex.pm
@@ -0,0 +1,14 @@
+package Acme::CPANModules::ConvertingFromRegex;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => 'Convert a regexp to various stuffs',
+    tags => ['task'],
+    entries => [
+    ],
+};
+
+1;
+# ABSTRACT:
