6d619ac123f5818bde933fd37daa6631356cac81
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-02-17 17:09:47 +0700

    Release v0.001

--- lib/Acme/CPANModules/ConvertingFromRegex.pm
+++ lib/Acme/CPANModules/ConvertingFromRegex.pm
@@ -4,11 +4,17 @@ package Acme::CPANModules::ConvertingFromRegex;
 # VERSION
 
 our $LIST = {
-    summary => 'Convert a regexp to various stuffs',
+    summary => 'Convert a regular expression to various stuffs',
     tags => ['task'],
     entries => [
+        {module=>'PPIx::Regexp', summary=>'To a PPI object'},
+        {module=>'Regexp::Stringify', summary=>'To Perl string representation'},
     ],
 };
 
 1;
 # ABSTRACT:
+
+=head1 prepend:SEE ALSO
+
+L<Acme::CPANModules::ConvertingToRegex>
